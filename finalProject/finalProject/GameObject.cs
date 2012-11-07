d20e3fd09977ed0411b2cfd91cbca9c4d2d37a6c
mschrod <mschrod@umich.edu>
2012-11-07 03:53:14 -0500

    Added start of base GameObject class

---
+++ finalProject/finalProject/GameObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace finalProject
+{
+    abstract class GameObject
+    {
+        private IRenderable mRenderable;
+        public Vector3 Position {
+            get { return mRenderable.Position; }
+            set { mRenderable.Position = value; }
+        }
+
+        public virtual void Render()
+        {
+            mRenderable.render();
+        }
+    }
+}
