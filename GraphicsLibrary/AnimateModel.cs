6e17f56e312c8758b0d2ea7ea96fe709316de132
Joshua Wehrly <josh.wehrly@gmail.com>
2012-11-07 12:05:37 -0500

    Added as-of-yet unimplemented graphics interface.
    Added New Graphics Library composed of an abstract base class Renderable
    as well as three stubbed out renderable derived types AnimatedModel
    (models with animations), InanimateModel (models without animation), and
    Terrain (heightmaps).

---
+++ GraphicsLibrary/AnimateModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace GraphicsLibrary
+{
+    class AnimateModel : Renderable
+    {
+        public AnimateModel(string modelName)
+        {
+
+        }
+
+        public void PlayAnimation(string animationName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Render()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ GraphicsLibrary/InanimateModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace GraphicsLibrary
+{
+    class InanimateModel : Renderable
+    {
+        public InanimateModel(string modelName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Render()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
