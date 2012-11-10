188473e1bfd150453704eb72da3ac949e341a19d
Jake Chrumka <chrumka@umich.edu>
2012-11-09 21:03:32 -0800

    Syncing for Kyle to reclone

--- MapEditor/MapEditor/MapEditor/MapEntity.cs
+++ MapEditor/MapEditor/MapEditor/MapEntity.cs
@@ -11,25 +11,30 @@ namespace MapEditor
     class MapEntity
     {
 
-        private const float speed = 10.0f;
+        private const float speed = 100.0f;
         private const float sensitivity = 1.0f;
         
         private Camera mCamera;
+        private Vector3 mPosition;
         private Vector2 mMovement;
         private Vector2 mOrientation;
 
         public MapEntity(Camera camera)
         {
             mCamera = camera;
-            mMovement = new Vector2(1.0f, 0.0f);
+            mPosition = new Vector3(0.0f, 0.0f, 0.0f);
+            mMovement = new Vector2(0.0f, 1.0f);
+            mOrientation = new Vector2(0.0f, 0.0f);
         }
 
         public void Update(GameTime gameTime)
         {
-            mCamera.MoveForward(speed * mMovement.Y * gameTime.ElapsedGameTime.Milliseconds);
-            mCamera.MoveRight(speed * mMovement.X * gameTime.ElapsedGameTime.Milliseconds);
-            mCamera.RotatePitch(sensitivity * mOrientation.Y * gameTime.ElapsedGameTime.Milliseconds);
-            mCamera.RotateYaw(sensitivity * mOrientation.X * gameTime.ElapsedGameTime.Milliseconds); 
+            Console.WriteLine(mPosition.X + "," + mPosition.Y + "," + mPosition.Z);
+            mPosition += speed * mMovement.Y * mCamera.Target * gameTime.ElapsedGameTime.Milliseconds;
+            mPosition += speed * mMovement.X * mCamera.Right * gameTime.ElapsedGameTime.Milliseconds;
+            //mCamera.RotatePitch(sensitivity * mOrientation.Y * gameTime.ElapsedGameTime.Milliseconds);
+            //mCamera.RotateYaw(sensitivity * mOrientation.X * gameTime.ElapsedGameTime.Milliseconds);
+            mCamera.Position = mPosition;
         }
 
     }
