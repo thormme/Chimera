dda5a7c0df4cdd03254844a63a37b7654bbdfa92
Kyle Fassnacht <kfoznot00@gmail.com>
2012-11-27 22:37:04 -0500

    Sensors are not working

--- finalProject/finalProject/DummyCreature.cs
+++ finalProject/finalProject/DummyCreature.cs
@@ -23,16 +23,12 @@ namespace finalProject
             : base(position, 2.0f, 0.25f, 10.0f, 10.0f, new HostileAI(), new InanimateModel("box"), MathHelper.PiOver4, 10.0f, 10.0f, new DummyPart(position + new Vector3(15.0f, 0.0f, 15.0f)))
         {
             Entity.Position = position;
+            mIncapacitated = false;
         }
 
         public override void Damage(int damage)
         {
             mIncapacitated = true;
-        }
-
-        public void Update(GameTime time)
-        {
-            base.Update(time);
         }
     }
 }
