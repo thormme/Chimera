187ea3ac0ceb9a996051132396b17201635ab019
Jake Chrumka <chrumka@umich.edu>
2012-11-29 01:51:15 -0800

    Added Cheetah!

--- finalProject/finalProject/Creatures/Cheetah.cs
+++ finalProject/finalProject/Creatures/Cheetah.cs
@@ -8,21 +8,21 @@ using GameConstructLibrary;
 
 namespace finalProject.Creatures
 {
-    class Rhino : NonPlayerCreature
+    class Cheetah : NonPlayerCreature
     {
-        public Rhino(Vector3 position)
+        public Cheetah(Vector3 position)
             : base(position,
                    2.0f,                            // Height
-                   3.0f,                            // Radius
-                   30.0f,                           // Mass
+                   2.0f,                            // Radius
+                   6.0f,                            // Mass
                    20.0f,                           // Sensitivity Radius
                    new IntimidationAI(),            // AI
-                   new InanimateModel("rhino"),     // Model
+                   new InanimateModel("cheetah"),   // Model
                    MathHelper.PiOver4,              // Vision Angle
                    10,                              // Listening Sensitivity
-                   1,                               // Sneak
-                   7,                               // Intimidation
-                   new /*RhinoHead*/DummyPart())    // Part
+                   10,                              // Sneak
+                   4,                               // Intimidation
+                   new /*CheetahHead*/DummyPart())  // Part
         {
 
         }
@@ -31,7 +31,10 @@ namespace finalProject.Creatures
         {
 
             List<Creature.PartBone> bones = new List<PartBone>();
-            bones.Add(PartBone.HeadCenterCap);
+            bones.Add(PartBone.LegFrontLeft1Cap);
+            bones.Add(PartBone.LegRearLeft1Cap);
+            bones.Add(PartBone.LegFrontRight1Cap);
+            bones.Add(PartBone.LegRearRight1Cap);
 
             return bones;
         }
