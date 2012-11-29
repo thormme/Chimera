3a3d9f79cfa28e04f0d0adaf0824adaebcc7947d
Kyle Fassnacht <kfoznot00@gmail.com>
2012-11-29 17:43:40 -0500

    Updated Rhino

--- finalProject/finalProject/Creatures/Rhino.cs
+++ finalProject/finalProject/Creatures/Rhino.cs
@@ -11,20 +11,21 @@ namespace finalProject.Creatures
     public class Rhino : NonPlayerCreature
     {
         public Rhino(Vector3 position)
-            : base(position,
-                   2.0f,                            // Height
-                   3.0f,                            // Radius
-                   30.0f,                           // Mass
-                   20.0f,                           // Sensitivity Radius
-                   new IntimidationAI(),            // AI
-                   new InanimateModel("rhino"),     // Model
-                   MathHelper.PiOver4,              // Vision Angle
-                   10,                              // Listening Sensitivity
-                   1,                               // Sneak
-                   7,                               // Intimidation
-                   50,								// Starting Health
-                   new /*RhinoHead*/DummyPart())    // Part
-            )
+            : base(
+                position,
+                2.0f,                            // Height
+                3.0f,                            // Radius
+                30.0f,                           // Mass
+                20.0f,                           // Sensitivity Radius
+                new IntimidationAI(),            // AI
+                new InanimateModel("rhino"),     // Model
+                MathHelper.PiOver4,              // Vision Angle
+                10,                              // Listening Sensitivity
+                1,                               // Sneak
+                7,                               // Intimidation
+                50,								// Starting Health
+                new /*RhinoHead*/DummyPart()    // Part
+                )
         {
             Bones.Add(PartBone.HeadCenterCap);
         }
