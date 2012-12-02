9e399835ecc79cffb1b5d8816b4ed96cd64dffb1
Kyle Fassnacht <kfoznot00@gmail.com>
2012-12-02 04:46:48 -0500

    forgot to actually commit the AIs.

---
+++ finalProject/finalProject/AI/IntimidationAI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GameConstructLibrary;
+using Microsoft.Xna.Framework;
+
+namespace finalProject
+{
+    public class IntimidationAI : AIController
+    {
+        protected Creature mMostIntimidatingCreature = null;
+
+        public override void Update(GameTime time, List<Creature> collidingCreatures)
+        {
+            base.Update(time, collidingCreatures);
+
+            // Find the most intimidating creature in the sensor.
+            mMostIntimidatingCreature = null;
+            foreach (Creature creature in collidingCreatures)
+            {
+                if (mMostIntimidatingCreature == null || creature.Intimidation > mMostIntimidatingCreature.Intimidation)
+                {
+                    mMostIntimidatingCreature = creature;
+                }
+            }
+        }
+    }
+}
---
+++ finalProject/finalProject/AI/KangarooAI.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using BEPUphysics;
+using GameConstructLibrary;
+using BEPUphysics.BroadPhaseEntries;
+using finalProject.Parts;
+
+namespace finalProject.AI
+{
+    class KangarooAI : PassiveAI
+    {
+        private double mJumpTimer = -1.0f;
+
+        public override void Damage(int damage, Creature source)
+        {
+            if (source != null)
+            {
+                FollowOrder(source);
+            }
+        }
+
+        protected virtual bool WithinStompRange(Creature creature)
+        {
+            if (mCreature.Position.Y < creature.Position.Y)
+            {
+                return false;
+            }
+
+            float distance = (creature.Position - mCreature.Position).Length();
+            if (distance > creature.CharacterController.BodyRadius + mCreature.CharacterController.BodyRadius)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        protected override void UsePartUpdate(GameTime time)
+        {
+            if (State == AIState.FollowCreature && WithinStompRange(mTargetCreature))
+            {
+                Vector3 downVector = new Vector3(0.0f, -1.0f, 0.0f);
+                Ray downRay = new Ray(mCreature.Position, downVector);
+                Func<BroadPhaseEntry, bool> filter = (bfe) => (!(bfe.Tag is Sensor) && bfe.Tag != mCreature);
+                RayCastResult result = new RayCastResult();
+                mCreature.World.Space.RayCast(downRay, filter, out result);
+                if (result.HitObject.Tag == mTargetCreature)
+                {
+                    Part part = ChoosePart();
+                    part.Use(downVector);
+                    part.FinishUse(downVector);
+                }
+            }
+
+            if (mUsingPart)
+            {
+                Vector3 targetDirection;
+                if (State == AIState.FollowCreature)
+                {
+                    targetDirection = mTargetCreature.Position - mCreature.Position;
+                }
+                else if (State == AIState.FleeCreature)
+                {
+                    targetDirection = mCreature.Position - mTargetCreature.Position;
+                }
+                else
+                {
+                    return;
+                }
+
+                KangarooLegs kLegs = (ChoosePart() as KangarooLegs);
+                if (mJumpTimer > kLegs.FullJumpTime)
+                {
+                    mJumpTimer += time.ElapsedGameTime.TotalSeconds;
+                    if (mJumpTimer / kLegs.FullJumpTime > targetDirection.Length() / mCreature.Sensor.Radius)
+                    {
+                        kLegs.FinishUse(targetDirection);
+                        mUsingPart = false;
+                    }
+                }
+                else
+                {
+                    ChoosePart().Use(targetDirection);
+                }
+            }
+        }
+    }
+}
