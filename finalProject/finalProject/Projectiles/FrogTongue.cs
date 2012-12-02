102fd5bc7b7e362c27e526b5222ca67eb3184d56
Jake Chrumka <chrumka@umich.edu>
2012-12-02 10:40:49 -0800

    fixing frog tongue.

--- finalProject/finalProject/Projectiles/FrogTongue.cs
+++ finalProject/finalProject/Projectiles/FrogTongue.cs
@@ -11,17 +11,20 @@ using BEPUphysics.Constraints.TwoEntity.Joints;
 using BEPUphysics.CollisionRuleManagement;
 using BEPUphysics;
 using BEPUphysics.Collidables;
+using BEPUphysics.Entities;
 
 namespace finalProject.Projectiles
 {
     public class FrogTongue : Projectile
     {
 
+        Vector3 mDestination;
         DistanceLimit mRopeLimit;
+        PhysicsObject mFakeAnchor;
         PhysicsObject mAnchorObject;
         bool mReachedDestination;
 
-        public FrogTongue(Actor owner, Vector3 direction)
+        public FrogTongue(Actor owner, Vector3 direction, Vector3 position)
             : base(
             new InanimateModel("box"),
             new Box(owner.Position, 0.5f, 0.5f, 0.5f, 0.5f),
@@ -31,6 +34,7 @@ namespace finalProject.Projectiles
             new Vector3(0.5f)
             )
         {
+            mDestination = position;
             Entity.IsAffectedByGravity = false;
             mReachedDestination = false;
         }
@@ -38,6 +42,14 @@ namespace finalProject.Projectiles
         public override void Update(GameTime time)
         {
             base.Update(time);
+
+            if (mRopeLimit == null && (mDestination - Position).Length() <= 2.0f)
+            {
+                mFakeAnchor = new PhysicsObject(null, new Sphere(mDestination, 1.0f));
+                mOwner.World.Add(mFakeAnchor);
+                CreateRope(mFakeAnchor);
+            }
+            
             if (mRopeLimit != null) // You have a connection
             {
                 Ray ray = new Ray(mOwner.Entity.Position, mAnchorObject.Entity.Position - mOwner.Entity.Position);
@@ -54,7 +66,6 @@ namespace finalProject.Projectiles
                         }
                         else if (!mReachedDestination)
                         {
-                            Console.WriteLine("here");
                             mRopeLimit.MaximumLength -= 20.0f * (float)time.ElapsedGameTime.TotalSeconds;
                         }
                     }
@@ -62,30 +73,54 @@ namespace finalProject.Projectiles
             }
         }
 
+        public override void InitialCollisionDetected(BEPUphysics.Collidables.MobileCollidables.EntityCollidable sender, Collidable other, BEPUphysics.NarrowPhaseSystems.Pairs.CollidablePairHandler collisionPair)
+        {
+            base.InitialCollisionDetected(sender, other, collisionPair);
+            // If hit physics object link projectile to it then grapple to projectile, otherwise set projectile to kinematic and grapple to projectile
+        }
+
         private void ReleaseTongue()
         {
+
             if (mRopeLimit != null)
             {
+                Console.WriteLine("releasing tongue");
+
                 mReachedDestination = false;
                 mOwner.World.Space.Remove(mRopeLimit);
                 mRopeLimit = null;
-                Unstick();
+                //Unstick();
             }
+
+            if (mFakeAnchor != null)
+            {
+                Console.WriteLine("removing fake anchor");
+                mOwner.World.Remove(mFakeAnchor);
+                mFakeAnchor = null;
+            }
+
         }
 
+        
         protected override void Hit(IGameObject gameObject)
         {
             PhysicsObject anchor = (gameObject as PhysicsObject);
             if (anchor != null)
             {
-                mAnchorObject = anchor;
-                CheckHits = false;
-                StickToObject(mAnchorObject);
-                mRopeLimit = new DistanceLimit(mOwner.Entity, mAnchorObject.Entity, mOwner.Entity.Position, mAnchorObject.Entity.Position, 0.0f, (mAnchorObject.Entity.Position - mOwner.Entity.Position).Length());
-                mRopeLimit.Bounciness = 0.8f;
-                mOwner.World.Space.Add(mRopeLimit);
+                CreateRope(anchor);
             }
         }
+
+        private void CreateRope(PhysicsObject anchor)
+        {
+            mAnchorObject = anchor;
+            CheckHits = false;
+            //StickToObject(mAnchorObject);
+            mRopeLimit = new DistanceLimit(mOwner.Entity, mAnchorObject.Entity, mOwner.Entity.Position, mAnchorObject.Entity.Position, 0.0f, (mAnchorObject.Entity.Position - mOwner.Entity.Position).Length());
+            mRopeLimit.Bounciness = 0.8f;
+            mOwner.World.Space.Add(mRopeLimit);
+        }
+         
 
         public override World World
         {
