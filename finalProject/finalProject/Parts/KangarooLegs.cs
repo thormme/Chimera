4214cbcd14eeeaf705c8a0c8d94afdab81c6bb87
Josh Wehrly <josh.wehrly@gmail.com>
2012-12-02 00:35:12 -0500

    Added fully animated Kangaroo and Rhino.

--- finalProject/finalProject/Parts/KangarooLegs.cs
+++ finalProject/finalProject/Parts/KangarooLegs.cs
@@ -30,7 +30,7 @@ namespace finalProject.Parts
             : base(
                 new Part.SubPart[] {
                     new SubPart(
-                        new InanimateModel("kangaroo_leftLeg"),
+                        new AnimateModel("kangaroo_leftLeg", "walk"),
                         new Creature.PartBone[] { 
                             Creature.PartBone.LegFrontLeft3Cap, 
                             Creature.PartBone.LegRearLeft3Cap,
@@ -44,7 +44,7 @@ namespace finalProject.Parts
                         new Vector3(1.0f, 1.0f, 1.0f)
                     ),
                     new SubPart(
-                        new InanimateModel("kangaroo_rightLeg"),
+                        new AnimateModel("kangaroo_rightLeg", "walk"),
                         new Creature.PartBone[] { 
                             Creature.PartBone.LegFrontRight3Cap, 
                             Creature.PartBone.LegRearRight3Cap,
@@ -68,11 +68,15 @@ namespace finalProject.Parts
             {
                 mJumpInUse = true;
                 mJumpStrengthTimer = 1.0;
+
+                PlayAnimation("charge", true);
             }
             else
             {
                 mPoundWaiting = true;
                 mPoundWaitTimer = poundWaitTime;
+
+                PlayAnimation("pound", true);
             }
         }
 
@@ -119,6 +123,11 @@ namespace finalProject.Parts
                 Creature.CharacterController.JumpSpeed /= mJumpMultiplier;
                 Creature.CharacterController.JumpForceFactor /= mJumpMultiplier;
             }
+
+            foreach (SubPart subPart in SubParts)
+            {
+                (subPart.Renderable as AnimateModel).Update(time);
+            }
         }
 
         public override void FinishUse(Vector3 direction)
@@ -138,6 +147,24 @@ namespace finalProject.Parts
 
                 Vector3 pushForward = direction * mJumpMultiplier * forwardJumpForce;
                 Creature.Entity.ApplyLinearImpulse(ref pushForward);
+                
+                PlayAnimation("jump", true);
+            }
+        }
+
+        public override void TryPlayAnimation(string animationName, bool isSaturated)
+        {
+            if (!mJumpInUse && !mPoundInUse && Creature.CharacterController.SupportFinder.HasSupport)
+            {
+                PlayAnimation(animationName, isSaturated);
+            }
+        }
+
+        protected override void PlayAnimation(string animationName, bool isSaturated)
+        {
+            foreach (SubPart subPart in SubParts)
+            {
+                (subPart.Renderable as AnimateModel).PlayAnimation(animationName, isSaturated);
             }
         }
 
