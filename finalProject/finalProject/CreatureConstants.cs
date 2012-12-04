8f76415f7e34d546d75c6ad5ed78f6e97a507a1c
Kyle Fassnacht <kfoznot00@gmail.com>
2012-12-04 03:33:36 -0500

    a;dilh

---
+++ finalProject/finalProject/CreatureConstants.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace finalProject
+{
+    public static class CreatureConstants
+    {
+        public const int PlayerIntimidation         = 5;
+        public const int BearIntimidation           = 8;
+        public const int CheetahIntimidation        = 4;
+        public const int CobraIntimidation          = 1;
+        public const int EagleIntimidation          = 2;
+        public const int FrilledLizardIntimidation  = 4;
+        public const int FrogIntimidation           = 4;// ??
+        public const int KangarooIntimidation       = 5;
+        public const int PenguinIntimidation        = 2;
+        public const int RhinoIntimidation          = 8;
+        public const int TurtleIntimidation         = 1;
+
+        public const float PlayerSensitivityRadius          = 10.0f;
+        public const float BearSensitivityRadius            = 15.0f;
+        public const float CheetahSensitivityRadius         = 15.0f;
+        public const float CobraSensitivityRadius           = 25.0f;
+        public const float EagleSensitivityRadius           = 20.0f;
+        public const float FrilledLizardSensitivityRadius   = 20.0f;
+        public const float FrogSensitivityRadius            = 20.0f;// ??
+        public const float KangarooSensitivityRadius        = 20.0f;
+        public const float PenguinSensitivityRadius         = 15.0f;
+        public const float RhinoSensitivityRadius           = 20.0f;
+        public const float TurtleSensitivityRadius          = 10.0f;
+
+        public const int PlayerStartingHealth           = 100;
+        public const int BearStartingHealth             = 100;
+        public const int CheetahStartingHealth          = 50;
+        public const int CobraStartingHealth            = 50;
+        public const int EagleStartingHealth            = 50;
+        public const int FrilledLizardStartingHealth    = 50;
+        public const int FrogStartingHealth             = 50;
+        public const int KangarooStartingHealth         = 50;
+        public const int PenguinStartingHealth          = 50;
+        public const int RhinoStartingHealth            = 100;
+        public const int TurtleStartingHealth           = 1;
+
+        public const int BearListeningSensitivity           = 10;
+        public const int CheetahListeningSensitivity        = 10;
+        public const int CobraListeningSensitivity          = 10;
+        public const int EagleListeningSensitivity          = 10;
+        public const int FrilledLizardListeningSensitivity  = 10;
+        public const int FrogListeningSensitivity           = 10;
+        public const int KangarooListeningSensitivity       = 10;
+        public const int PenguinListeningSensitivity        = 10;
+        public const int RhinoListeningSensitivity          = 10;
+        public const int TurtleListeningSensitivity         = 10;
+
+        public const int PlayerSneak        = 5;
+        public const int BearSneak          = 5;
+        public const int CheetahSneak       = 5;
+        public const int CobraSneak         = 5;
+        public const int EagleSneak         = 5;
+        public const int FrilledLizardSneak = 5;
+        public const int FrogSneak          = 5;
+        public const int KangarooSneak      = 5;
+        public const int PenguinSneak       = 5;
+        public const int RhinoSneak         = 5;
+        public const int TurtleSneak        = 1;
+    }
+}
