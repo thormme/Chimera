66c17ba3625b04c962803f3889440191273ba23e
Jake Chrumka <chrumka@umich.edu>
2013-01-27 16:31:44 -0800

    Working on player part menu.

--- finalProject/finalProject/PlayerInfo.cs
+++ finalProject/finalProject/PlayerInfo.cs
@@ -8,5 +8,41 @@ namespace finalProject
     public class PlayerInfo
     {
 
+        public List<Type> Parts
+        {
+            get
+            {
+                return mAvailableParts;
+            }
+            private set
+            {
+                mAvailableParts = value;
+            }
+        }
+        private List<Type> mAvailableParts = new List<Type>();
+
+        public PlayerInfo()
+        {
+
+        }
+
+        public PlayerInfo(List<Type> availableParts)
+        {
+            mAvailableParts = availableParts;
+        }
+
+        public void AddPart(Type partType)
+        {
+            if (!mAvailableParts.Contains(partType))
+            {
+                mAvailableParts.Add(partType);
+            }
+        }
+
+        public void RemovePart(Type partType)
+        {
+            mAvailableParts.Remove(partType);
+        }
+
     }
 }
