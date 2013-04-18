9e11d94d840c63a390b9b325ed72de4ac2303369
mschrod <mschrod@umich.edu>
2013-04-18 18:49:25 -0400

    Added ObjectPlacementForm classes

---
+++ WorldEditor/WorldEditor/Dialogs/ObjectPlacementPanel.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace WorldEditor.Dialogs
+{
+    partial class ObjectPlacementPanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ObjectTree = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // ObjectTree
+            // 
+            this.ObjectTree.Location = new System.Drawing.Point(3, 3);
+            this.ObjectTree.Name = "ObjectTree";
+            this.ObjectTree.Size = new System.Drawing.Size(229, 214);
+            this.ObjectTree.TabIndex = 0;
+            // 
+            // ObjectPlacementPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ObjectTree);
+            this.Name = "ObjectPlacementPanel";
+            this.Size = new System.Drawing.Size(235, 379);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TreeView ObjectTree;
+
+    }
+}
